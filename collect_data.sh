b65d2effe0a0470b955f530abcfdb058785acf6f
pchez <priscillaccheng@gmail.com>
2018-03-14 18:25:30 +0000

    working training

--- collect_data.sh
+++ collect_data.sh
@@ -9,23 +9,25 @@ trap collectData USR1
 trap run USR2
 trap 'trap - INT && finished=1' INT TERM EXIT
 #trap "trap - INT && kill -- -$(ps -o pgid=$PID | grep -o [0-9]*) && kill $$" INT TERM EXIT
+LABEL=0
 
 collectData()
 {
     errcho "collect_data: Collecting data for training..."
 
-#    gatttool -b C0:85:40:31:4D:48 -t random --char-write-req --handle=0x0012 --value=0100 --listen > motion_raw.txt &
-#    GATT_PID=$!
-#    errcho PID of gatttool: $GATT_PID
-#    sleep 5
-#    kill -TERM $GATT_PID
-#    SLEEP_COLLECT_PID=$!
-#
-#    errcho "collect_data: finished, converting...\n"    
-#    sed 's/Notification handle = 0x0011 value: //' < motion_raw.txt > motion_preprocessed.txt
-#    sed -i '1d' motion_preprocessed.txt
-#    python convert_data.py motion_preprocessed.txt motion_converted.txt 1
-#    #sed -i "s/Characteristic value was written successfully//"  motion_raw_test.txt
+    gatttool -b C0:85:40:31:4D:48 -t random --char-write-req --handle=0x0012 --value=0100 --listen > motion_raw.txt &
+    GATT_PID=$!
+    errcho PID of gatttool: $GATT_PID
+    sleep 5
+    kill -TERM $GATT_PID
+    SLEEP_COLLECT_PID=$!
+
+    errcho "collect_data: finished, converting...\n"    
+    sed 's/Notification handle = 0x0011 value: //' < motion_raw.txt > motion_preprocessed.txt
+    sed -i '1d' motion_preprocessed.txt
+    LABEL=`expr $LABEL + 1`
+    python convert_data.py motion_preprocessed.txt motion_converted.txt $LABEL
+    #sed -i "s/Characteristic value was written successfully//"  motion_raw_test.txt
     sleep 1
     kill -USR1 $PARENT_PID
     
